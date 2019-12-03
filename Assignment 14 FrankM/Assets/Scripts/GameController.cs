f222e01878764b33e9d974937404302b98ac7c5d
Frank <fmarinellicollege@gmail.com>
2019-12-03 16:55:26 -0500

    Homework complete

---
+++ Assignment 14 FrankM/Assets/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("If you are seeing this, then everything is working correctly. Hopefully.");
+        Analytics.CustomEvent("startingLevel");
+        
+    }
+
+
+    public void BigButtonPressed()
+    {
+        Analytics.CustomEvent("primaryUI", new Dictionary<string, object>
+        {
+            { "buttonPressed", "mainButton" },
+            { "aValue", "timesPressed" }
+        });
+
+        
+    }
+
+    public void SecretButtonPressed()
+    {
+        Analytics.CustomEvent("locatedSecretButton");
+    }
+
+    public void BeautifulButtonPressed()
+    {
+        Analytics.CustomEvent("secondaryUI", new Vector3(Random.Range(0f, 10f), Random.Range(0f, 10f), Random.Range(0f, 10f))); 
+    }
+}
